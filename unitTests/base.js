e9730e833dc0e2cd77d200dc86597ef9c9513bad
Rob Evans <rob@irrelon.com>
2014-03-17 14:06:24 +0000

    Updated data with more info for sort test coverage

--- unitTests/base.js
+++ unitTests/base.js
@@ -33,7 +33,8 @@ var db,
 		log: {
 			val: 7
 		},
-		orgId: "1"
+		orgId: "1",
+		friends: ["3", "4"]
 	}, {
 		_id: '3',
 		name: 'Kat',
@@ -49,7 +50,8 @@ var db,
 		log: {
 			val: 1
 		},
-		orgId: "2"
+		orgId: "2",
+		friends: ["2"]
 	}, {
 		_id: '4',
 		name: 'Dean',
@@ -65,23 +67,82 @@ var db,
 		log: {
 			val: 2
 		},
-		orgId: "3"
+		orgId: "3",
+		friends: ["2", "3"]
 	}],
 	organisationsData = [{
 		"_id": "1",
-		"name": "Organisation 1"
+		"name": "Organisation 1",
+		"industry": "it",
+		"profit": 200,
+		"type": "alpha"
 	}, {
 		"_id": "2",
-		"name": "Organisation 1"
+		"name": "Organisation 2",
+		"industry": "it",
+		"profit": 135,
+		"type": "beta"
 	}, {
 		"_id": "3",
-		"name": "Organisation 1"
+		"name": "Organisation 3",
+		"industry": "it",
+		"profit": 135,
+		"type": "delta"
 	}, {
 		"_id": "4",
-		"name": "Organisation 1"
+		"name": "Organisation 4",
+		"industry": "it",
+		"profit": 135,
+		"type": "cappa"
 	}, {
 		"_id": "5",
-		"name": "Organisation 1"
+		"name": "Organisation 5",
+		"industry": "construction",
+		"profit": 340
+	}, {
+		"_id": "6",
+		"name": "Organisation 6",
+		"industry": "construction",
+		"profit": 980
+	}, {
+		"_id": "7",
+		"name": "Organisation 7",
+		"industry": "construction",
+		"profit": 45
+	}, {
+		"_id": "8",
+		"name": "Organisation 8",
+		"industry": "construction",
+		"profit": 27
+	}, {
+		"_id": "9",
+		"name": "Organisation 9",
+		"industry": "construction",
+		"profit": 664
+	}, {
+		"_id": "10",
+		"name": "Organisation 10",
+		"industry": "it",
+		"profit": 780,
+		"type": "cappa"
+	}, {
+		"_id": "11",
+		"name": "Organisation 11",
+		"industry": "it",
+		"profit": 1002,
+		"type": "alpha"
+	}, {
+		"_id": "12",
+		"name": "Organisation 12",
+		"industry": "it",
+		"profit": 1002,
+		"type": "xray"
+	}, {
+		"_id": "13",
+		"name": "Organisation 13",
+		"industry": "it",
+		"profit": 1002,
+		"type": "gamma"
 	}];
 
 var buildUp = function () {
