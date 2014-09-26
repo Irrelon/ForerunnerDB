ece5ac86f63fc4eb615dc9798518f8a8671883e7
Rob Evans <rob@irrelon.com>
2014-09-26 15:01:11 +0100

    Code style updates

--- js/lib/LocalForage.js
+++ js/lib/LocalForage.js
@@ -1,8 +1,6 @@
 /*
-  Extends the Persist Class to support LocalForage instead of localstorage
-*/
-
-
+ Extends the Persist Class to support LocalForage instead of localStorage
+ */
 var Shared = require('./Shared');
 
 // dependencies for extending with localforage support
@@ -14,60 +12,71 @@ var PersistSave = Persist.prototype.save;
 var PersistLoad = Persist.prototype.load;
 var PersistDrop = Persist.prototype.drop;
 
-Persist.prototype.init = function(){
-
-  if (db.isClient()) {
-    if (Storage !== undefined) {
-      this.mode('localForage');
-    }
-  }
+Persist.prototype.init = function () {
+	if (db.isClient()) {
+		if (Storage !== undefined) {
+			this.mode('localForage');
+		}
+	}
 
 };
 
 Persist.prototype.save = function (key, data, callback) {
-  var result;
+	var result;
 
-  switch (this.mode()) {
-    case 'localForage':
-      result = localforage.setItem(key, data);
+	switch (this.mode()) {
+		case 'localForage':
+			result = localforage.setItem(key, data);
 
-      if(callback){ result.then(callback); }
-      else { return result; }
-      break;
-    default:
-      return PersistSave.apply(this, arguments);
-  }
+			if (callback) {
+				result.then(callback);
+			}
+			else {
+				return result;
+			}
+			break;
+		default:
+			return PersistSave.apply(this, arguments);
+	}
 
 };
 
 Persist.prototype.load = function (key, callback) {
-  var result;
+	var result;
 
-  switch (this.mode()) {
-    case 'localForage':
-      result = localforage.getItem(key);
+	switch (this.mode()) {
+		case 'localForage':
+			result = localforage.getItem(key);
 
-      if(callback){ result.then(callback); }
-      else { return result; }
-      break;
-    default:
-      return PersistLoad.apply(this, arguments);
-  }
-  
+			if (callback) {
+				result.then(callback);
+			}
+			else {
+				return result;
+			}
+			break;
+		default:
+			return PersistLoad.apply(this, arguments);
+	}
+
 };
 
 Persist.prototype.drop = function (key, callback) {
-  var result;
+	var result;
 
-  switch (this.mode()) {
-    case 'localForage':
-      result = localforage.removeItem(key);
-      
-      if(callback){ result.then(callback); }
-      else { return result; }
-      break;
-    default:
-      return PersistDrop.apply(this, arguments);
-  }
-  
+	switch (this.mode()) {
+		case 'localForage':
+			result = localforage.removeItem(key);
+
+			if (callback) {
+				result.then(callback);
+			}
+			else {
+				return result;
+			}
+			break;
+		default:
+			return PersistDrop.apply(this, arguments);
+	}
+
 };
