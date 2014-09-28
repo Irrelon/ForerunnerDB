5dcb9ae63042245e5c6b04b44f4ddb885088a7b8
Naman Goel <naman34@gmail.com>
2014-09-28 21:45:18 +0530

    fixed problems with localforage

--- js/lib/LocalForage.js
+++ js/lib/LocalForage.js
@@ -6,13 +6,14 @@ var Shared = require('./Shared');
 // dependencies for extending with localforage support
 var localforage = require('localforage');
 var Persist = require('./Persist');
+//debugger;
 
 var PersistInit = Persist.prototype.init;
 var PersistSave = Persist.prototype.save;
 var PersistLoad = Persist.prototype.load;
 var PersistDrop = Persist.prototype.drop;
 
-Persist.prototype.init = function () {
+Persist.prototype.init = function (db) {
 	if (db.isClient()) {
 		if (Storage !== undefined) {
 			this.mode('localForage');
@@ -26,10 +27,11 @@ Persist.prototype.save = function (key, data, callback) {
 
 	switch (this.mode()) {
 		case 'localForage':
+
 			result = localforage.setItem(key, data);
 
 			if (callback) {
-				result.then(callback);
+				result.then(function(data){ callback(null, data); });
 			}
 			else {
 				return result;
@@ -46,10 +48,11 @@ Persist.prototype.load = function (key, callback) {
 
 	switch (this.mode()) {
 		case 'localForage':
+
 			result = localforage.getItem(key);
 
 			if (callback) {
-				result.then(callback);
+				result.then(function(data){ callback(null, data); });
 			}
 			else {
 				return result;
@@ -66,10 +69,11 @@ Persist.prototype.drop = function (key, callback) {
 
 	switch (this.mode()) {
 		case 'localForage':
+
 			result = localforage.removeItem(key);
 
 			if (callback) {
-				result.then(callback);
+				result.then(function(data){ callback(null, data); });
 			}
 			else {
 				return result;
@@ -80,3 +84,5 @@ Persist.prototype.drop = function (key, callback) {
 	}
 
 };
+
+module.exports = Persist;
