bbd33ca3f6426d9ecea76488fff9fa54e07b6eb8
Rob Evans <rob@irrelon.com>
2014-10-14 12:59:41 +0100

    Activebucket now fully working

--- js/lib/ActiveBucket.js
+++ js/lib/ActiveBucket.js
@@ -7,21 +7,16 @@ var ActiveBucket = function (orderBy) {
 
 	this._primaryKey = '_id';
 	this._keyArr = [];
-	this._bucketData = {};
+	this._data = [];
+	this._objLookup = {};
 	this._count = 0;
-	this._order = [];
-	this._ref = {};
 
 	for (sortKey in orderBy) {
 		if (orderBy.hasOwnProperty(sortKey)) {
-			/*sortSingleObj = {};
-			sortSingleObj[sortKey] = orderBy[sortKey];
-			sortSingleObj.___fdbKey = sortKey;*/
-			this._keyArr.push(sortKey);
-
-			bucketData = this._bucketData[sortKey] = new BucketData();
-			bucketData._path = sortKey;
-			bucketData._dir = orderBy[sortKey];
+			this._keyArr.push({
+				key: sortKey,
+				dir: orderBy[sortKey]
+			});
 		}
 	}
 };
@@ -29,168 +24,155 @@ var ActiveBucket = function (orderBy) {
 Shared.addModule('ActiveBucket', ActiveBucket);
 Shared.synthesize(ActiveBucket.prototype, 'primaryKey');
 
-ActiveBucket.prototype.add = function (obj) {
-	var index,
-		docIndex,
-		arr = this._keyArr,
-		count = arr.length;
-
-	for (index = 0; index < count; index++) {
-		this._bucketData[arr[index]].add(obj);
+ActiveBucket.prototype.qs = function (obj, arr, item, fn) {
+	if (!arr.length) {
+		return 0;
 	}
 
-	// Create a reference to the document
-	this._addRef(obj);
+	var midwayIndex,
+		lookupItem,
+		result,
+		start = 0,
+		end = arr.length - 1,
+		count = 0;
 
-	this._count++;
+	while (count < 100 && end >= start) {
+		midwayIndex = Math.floor((start + end) / 2);
+		lookupItem = arr[midwayIndex];
+		result = fn(this, obj, item, lookupItem);
+
+		if (result > 0) {
+			start = midwayIndex + 1;
+		}
+
+		if (result < 0) {
+			end = midwayIndex - 1;
+		}
+		count++;
+	}
+
+	if (result > 0) {
+		return midwayIndex + 1;
+	} else {
+		return midwayIndex;
+	}
+
 };
 
-ActiveBucket.prototype._addRef = function (obj) {
-	var index,
-		arr = this._keyArr,
-		arrCount = arr.length,
-		currRef = this._ref,
-		key,
-		keyValue;
+ActiveBucket.prototype._sortFunc = function (sorter, obj, a, b) {
+	var aVals = a.split('.:.'),
+		bVals = b.split('.:.'),
+		arr = sorter._keyArr,
+		count = arr.length,
+		index,
+		sortType,
+		castType;
 
-	for (index = 0; index < arrCount; index++) {
-		key = arr[index];
-		keyValue = obj[key];
+	for (index = 0; index < count; index++) {
+		sortType = arr[index];
+		castType = typeof obj[sortType.key];
 
-		if (index === arrCount - 1) {
-			// This is the last key, assign the object
-			currRef = currRef[keyValue] = currRef[keyValue] || [];
+		if (castType === 'number') {
+			aVals[index] = Number(aVals[index]);
+			bVals[index] = Number(bVals[index]);
+		}
 
-			// Push the object to the array
-			currRef.push(obj);
-		} else {
-			currRef = currRef[keyValue] = currRef[keyValue] || {};
+		// Check for non-equal items
+		if (aVals[index] !== bVals[index]) {
+			// Return the sorted items
+			if (sortType.dir === 1) {
+				return sorter.sortAsc(aVals[index], bVals[index]);
+			}
+
+			if (sortType.dir === -1) {
+				return sorter.sortDesc(aVals[index], bVals[index]);
+			}
 		}
 	}
+};
+
+ActiveBucket.prototype.insert = function (obj) {
+	var key,
+		keyIndex;
+
+	key = this.documentKey(obj);
+	keyIndex = this._data.indexOf(key);
+
+	if (keyIndex === -1) {
+		// Insert key
+		keyIndex = this.qs(obj, this._data, key, this._sortFunc);
+
+		this._data.splice(keyIndex, 0, key);
+	} else {
+		this._data.splice(keyIndex, 0, key);
+	}
+
+	this._objLookup[obj[this._primaryKey]] = key;
+
+	this._count++;
+	return keyIndex;
 };
 
 ActiveBucket.prototype.remove = function (obj) {
-	this._bucketData.remove(obj);
-	this._count--;
+	var key,
+		keyIndex;
+
+	key = this._objLookup[obj[this._primaryKey]];
+
+	if (key) {
+		keyIndex = this._data.indexOf(key);
+		this._data.splice(keyIndex, 0, key);
+		delete this._objLookup[obj[this._primaryKey]];
+
+		this._count--;
+		return true;
+	}
+
+	return false;
 };
 
 ActiveBucket.prototype.index = function (obj) {
-	var index,
-		positionArr = [],
-		arr = this._keyArr,
-		count = arr.length,
-		docIndex,
-		key,
-		currRef = this._ref,
-		finalIndex;
+	var key,
+		keyIndex;
 
-	for (index = 0; index < count; index++) {
-		key = arr[index];
-		currRef = currRef[obj[key]];
+	key = this.documentKey(obj);
+	keyIndex = this._data.indexOf(key);
 
-		docIndex = this._bucketData[key].index(obj);
-		docIndex = index === 0 ? docIndex : ("000000" + docIndex).slice(-6);
-
-		if (index === count - 1) {
-			// Assign the array index to the index
-			finalIndex = currRef.indexOf(obj);
-
-			if (finalIndex === -1) {
-				finalIndex = currRef.length;
-			}
-
-			docIndex += '.' + ("000000" + finalIndex).slice(-6);
-		}
-
-		positionArr.push(docIndex);
+	if (keyIndex === -1) {
+		// Get key index
+		keyIndex = this.qs(obj, this._data, key, this._sortFunc);
 	}
 
-	return positionArr.join('.');
+	return keyIndex;
+};
+
+ActiveBucket.prototype.documentKey = function (obj) {
+	var key = '',
+		arr = this._keyArr,
+		count = arr.length,
+		index,
+		sortType;
+
+	for (index = 0; index < count; index++) {
+		sortType = arr[index];
+		if (key) {
+			key += '.:.';
+		}
+
+		key += obj[sortType.key];
+	}
+
+	// Add the unique identifier on the end of the key
+	key += '.:.' + obj[this._primaryKey];
+
+	return key;
 };
 
 ActiveBucket.prototype.count = function () {
 	return this._count;
 };
 
-BucketData = function () {
-	this._path = ''; // The name of the key to sort on
-	this._dir = 1; // The direction to sort in (defaults to ascending)
-	this._data = {}; // The bucket's data
-	this._order = []; // The sorted keys
-	this._count = 0; // The overall number of objects held in reference
-};
-
-BucketData.prototype.add = function (obj) {
-	var keyValue = obj[this._path];
-
-	// Check if the object has the path we need to sort on
-	if (keyValue !== undefined) {
-		// Check if this is a new key or existing one
-		if (this._data[keyValue] === undefined) {
-			// Add the new key as an object we can store further data against
-			this._data[keyValue] = 1;
-
-			// Add the key to the order array
-			this._order.push(keyValue);
-
-			// Sort the key array so it's always up to date
-			if (this._dir === 1) {
-				// Sort ascending
-				this._order.sort(this._sortAsc);
-			} else if (this._dir === -1) {
-				// Sort descending
-				this._order.sort(this._sortDesc);
-			}
-		} else {
-			// Add to the existing reference count
-			this._data[keyValue]++;
-		}
-
-		this._count++;
-	}
-};
-
-BucketData.prototype.remove = function (obj) {
-	var keyValue = obj[this._path],
-		keyIndex;
-
-	// Check if the object has the path we need to sort on
-	if (keyValue !== undefined) {
-		// Check if this is a new key or existing one
-		if (this._data[keyValue]) {
-			// Dereference this object from the ref counter
-			this._data[keyValue]--;
-
-			// Check if the key is no longer referenced
-			if (this._data[keyValue] === 0) {
-				// The key is no longer referenced, remove it
-				keyIndex = this._order.indexOf(keyValue);
-
-				if (keyIndex > -1) {
-					this._order.splice(keyIndex, 1);
-				}
-
-				delete this._data[keyValue];
-			}
-		}
-
-		this._count--;
-	}
-};
-
-BucketData.prototype.index = function (obj) {
-	return this._order.indexOf(obj[this._path]);
-};
-
-BucketData.prototype.count = function  () {
-	return this._count;
-};
-
-BucketData.prototype.keyCount = function () {
-	return this._order.length;
-};
-
-BucketData.prototype._sortAsc = function (a, b) {
+ActiveBucket.prototype.sortAsc = function (a, b) {
 	if (typeof(a) === 'string' && typeof(b) === 'string') {
 		return a.localeCompare(b);
 	} else {
@@ -204,9 +186,9 @@ BucketData.prototype._sortAsc = function (a, b) {
 	return 0;
 };
 
-BucketData.prototype._sortDesc = function (a, b) {
+ActiveBucket.prototype.sortDesc = function (a, b) {
 	if (typeof(a) === 'string' && typeof(b) === 'string') {
-		return a.localeCompare(b) === true ? -1 : 1;
+		return b.localeCompare(a);
 	} else {
 		if (a > b) {
 			return -1;
@@ -218,4 +200,4 @@ BucketData.prototype._sortDesc = function (a, b) {
 	return 0;
 };
 
-Shared.addModule('BucketData', BucketData);
+module.exports = ActiveBucket;
