7697317be66fad501a3a4e05e3a22e8dcd2dd37f
Rob Evans <rob@irrelon.com>
2014-11-18 10:52:48 +0000

    Fixed incorrect core version string

--- js/lib/Core.js
+++ js/lib/Core.js
@@ -33,7 +33,7 @@ var Core = function () {
 Core.prototype.init = function () {
 	this._collection = {};
 	this._debug = {};
-	this._version = '1.2.7';
+	this._version = '1.2.10';
 };
 
 Core.prototype.moduleLoaded = Overload({
