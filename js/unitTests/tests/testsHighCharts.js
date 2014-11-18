13f3da59e95f04cba9ed90e3cf7fd730c64c9573
Rob Evans <rob@irrelon.com>
2014-11-18 11:18:12 +0000

    Fixed bug in chart updating

--- js/unitTests/tests/testsHighCharts.js
+++ js/unitTests/tests/testsHighCharts.js
@@ -22,22 +22,50 @@ ForerunnerDB.moduleLoaded('Highchart', function () {
 		}]);
 
 		// Create a pie chart on the element with the id "demo-chart"
-		coll.lineChart('#demo-chart', 'type', 'date', 'val', {
+		/*coll.lineChart('#demo-chart', 'type', 'date', 'val', {
 			chartOptions: {
 				title: {
 					text: 'Jam Stores Over Time'
 				}
 			}
-		});
+		});*/
 
-		/*coll.pieChart('#demo-chart', 'name', 'val', 'Food');*/
-		//coll.lineChart('#demo-chart', 'type', 'date', 'val');
+		coll.pieChart('#demo-chart', 'name', 'val', 'Food');
+		/*coll.lineChart('#demo-chart', 'type', 'date', 'val');*/
 		/*coll.areaChart('#demo-chart', 'type', 'date', 'val');*/
 		/*coll.columnChart('#demo-chart', 'type', 'date', 'val');*/
 		/*coll.barChart('#demo-chart', 'type', 'date', 'val');*/
 		/*coll.stackedBarChart('#demo-chart', 'type', 'date', 'val');*/
 
-		ok(true, 'No errors');
+		ok(true, 'Creating ok');
+
+		// Attempt to insert data
+		coll.insert({
+			type: 'Jam',
+			date: String(new Date('2014-09-16')).substr(0, 15),
+			val: 100
+		});
+
+		ok(true, 'Inserting ok');
+
+		// Attempt to update data
+		coll.update({
+			type: 'Jam',
+			date: String(new Date('2014-09-16')).substr(0, 15)
+		}, {
+			val: 150
+		});
+
+		ok(true, 'Updating ok');
+
+		// Attempt to remove data
+		coll.remove({
+			type: 'Jam',
+			date: String(new Date('2014-09-16')).substr(0, 15)
+		});
+
+		ok(true, 'Removing ok');
+
 		base.domDown();
 		base.dbDown();
 	});
