8515509cc09c26ee75fe55e980e4bddb00e20374
Rob Evans <rob@irrelon.com>
2015-01-20 13:08:48 +0000

    Version up

--- js/lib/Core.js
+++ js/lib/Core.js
@@ -34,7 +34,7 @@ Core.prototype.init = function (name) {
 	this._name = name;
 	this._collection = {};
 	this._debug = {};
-	this._version = '1.2.25';
+	this._version = '1.2.26';
 };
 
 Core.prototype.moduleLoaded = Overload({
