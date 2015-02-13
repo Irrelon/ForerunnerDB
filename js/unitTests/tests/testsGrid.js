6e23e18744d1b6f0b3806554cea6f8031442e62a
Rob Evans <rob@irrelon.com>
2015-02-13 16:15:51 +0000

    New grid module tests

---
+++ js/unitTests/tests/testsGrid.js
@@ -0,0 +1,42 @@
+test('Grid - Grid() :: Instantiate a grid', function () {
+	base.dbUp();
+
+	var grid = db.grid('.moo');
+
+	ok (grid instanceof db.shared.modules.Grid, 'Grid instantiated from correct class');
+
+	base.dbDown();
+});
+
+test('Grid - Grid() :: Bind a grid', function () {
+	base.dbUp();
+	base.domUp();
+
+	var coll = db.collection('gridTest').truncate(),
+		view = db.view('gridTest').from(coll),
+		grid = view.grid('#testTarget2', '#gridTable');
+
+	coll.setData([{
+		firstName: 'Fred',
+		lastName: 'Jones',
+		age: 15
+	}, {
+		firstName: 'Jim',
+		lastName: 'Franks',
+		age: 34
+	}, {
+		firstName: 'Tilly',
+		lastName: 'Monsoon',
+		age: 52
+	}, {
+		firstName: 'Arbit',
+		lastName: 'Frogsbottom',
+		age: 63
+	}]);
+
+	ok (grid, 'Grid active');
+	//grid.drop();
+
+	//base.domDown();
+	base.dbDown();
+});
