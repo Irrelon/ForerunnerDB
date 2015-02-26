c88becf476089c69e00b1847493ee532309bd5a6
Rob Evans <rob@irrelon.com>
2015-02-26 12:20:28 +0000

    Added some jshint ignores

--- js/lib/Crc.js
+++ js/lib/Crc.js
@@ -6,7 +6,7 @@ var crcTable = (function () {
 		c = n;
 
 		for (k = 0; k < 8; k++) {
-			c = ((c & 1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+			c = ((c & 1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1)); // jshint ignore:line
 		}
 
 		crcTable[n] = c;
@@ -16,12 +16,12 @@ var crcTable = (function () {
 }());
 
 module.exports = function(str) {
-	var crc = 0 ^ (-1),
+	var crc = 0 ^ (-1), // jshint ignore:line
 		i;
 
 	for (i = 0; i < str.length; i++) {
-		crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+		crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF]; // jshint ignore:line
 	}
 
-	return (crc ^ (-1)) >>> 0;
+	return (crc ^ (-1)) >>> 0; // jshint ignore:line
 };
