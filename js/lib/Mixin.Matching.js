93661cd121e91cf268be8e118274cb7023c58b69
Rob Evans <rob@irrelon.com>
2015-03-04 10:07:31 +0000

    Changed string matching to use localeCompare like we already do with sorting, in preparation for #35

--- js/lib/Mixin.Matching.js
+++ js/lib/Mixin.Matching.js
@@ -24,9 +24,18 @@ var Matching = {
 		if ((sourceType === 'string' || sourceType === 'number') && (testType === 'string' || testType === 'number')) {
 			// The source and test data are flat types that do not require recursive searches,
 			// so just compare them and return the result
-			if (source !== test) {
-				matchedAll = false;
+			if (sourceType === 'number') {
+				// Number comparison
+				if (source !== test) {
+					matchedAll = false;
+				}
+			} else {
+				// String comparison
+				if (source.localeCompare(test)) {
+					matchedAll = false;
+				}
 			}
+
 		} else {
 			for (i in test) {
 				if (test.hasOwnProperty(i)) {
