6bdefb1ca80713425d722943df0861bb7ff546c0
irrelon <rob@irrelon.com>
2015-04-20 15:48:24 +0100

    Fixed bug in queryAdd() method not defaulting the overwrite argument to true when none passed

--- js/lib/View.js
+++ js/lib/View.js
@@ -120,7 +120,7 @@ View.prototype.from = function (collection) {
 		if (this._from) {
 			// Remove the listener to the drop event
 			this._from.off('drop', this._collectionDroppedWrap);
-			this._from._removeView(this);
+			delete this._from;
 		}
 
 		if (typeof(collection) === 'string') {
@@ -526,6 +526,8 @@ View.prototype.queryData = function (query, options, refresh) {
  * once the operation is complete. Defaults to true.
  */
 View.prototype.queryAdd = function (obj, overwrite, refresh) {
+	this._querySettings.query = this._querySettings.query || {};
+
 	var query = this._querySettings.query,
 		i;
 
@@ -533,7 +535,7 @@ View.prototype.queryAdd = function (obj, overwrite, refresh) {
 		// Loop object properties and add to existing query
 		for (i in obj) {
 			if (obj.hasOwnProperty(i)) {
-				if (query[i] === undefined || (query[i] !== undefined && overwrite)) {
+				if (query[i] === undefined || (query[i] !== undefined && overwrite !== false)) {
 					query[i] = obj[i];
 				}
 			}
@@ -669,6 +671,8 @@ View.prototype.refresh = function () {
 			//var transformedData = this._privateData.find();
 			// TODO: Shouldn't this data get passed into a transformIn first?
 			// TODO: This breaks linking because its passing decoupled data and overwriting non-decoupled data
+			// TODO: Is this even required anymore? After commenting it all seems to work
+			// TODO: Might be worth setting up a test to check trasforms and linking then remove this if working?
 			//jQuery.observable(pubData._data).refresh(transformedData);
 		}
 	}
@@ -750,6 +754,9 @@ View.prototype.privateData = function () {
  * same as the private data the view holds. If transforms are
  * applied then the public data will contain the transformed version
  * of the private data.
+ *
+ * The public data collection is also used by data binding to only
+ * changes to the publicData will show in a data-bound element.
  */
 View.prototype.publicData = function () {
 	if (this._transformEnabled) {
