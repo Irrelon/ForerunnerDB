f2d3dcee7560404a3e5072f8d5cffe3f15daa083
irrelon <rob@irrelon.com>
2015-04-28 13:52:00 +0100

    Added overloaded function signatures to the Core.prototype.collection() method

--- js/lib/Collection.js
+++ js/lib/Collection.js
@@ -12,7 +12,8 @@ var Shared,
 	Path,
 	IndexHashMap,
 	IndexBinaryTree,
-	Crc;
+	Crc,
+	Overload;
 
 Shared = require('./Shared');
 
@@ -76,6 +77,7 @@ IndexHashMap = require('./IndexHashMap');
 IndexBinaryTree = require('./IndexBinaryTree');
 Crc = require('./Crc');
 Core = Shared.modules.Core;
+Overload = require('./Overload');
 
 /**
  * Returns a checksum of a string.
@@ -2774,33 +2776,115 @@ Collection.prototype.diff = function (collection) {
 	return diff;
 };
 
-/**
- * Get a collection by name. If the collection does not already exist
- * then one is created for that name automatically.
- * @param {String} collectionName The name of the collection.
- * @param {String=} primaryKey Optional primary key to specify the primary key field on the collection
- * objects. Defaults to "_id".
- * @returns {Collection}
- */
-Core.prototype.collection = function (collectionName, primaryKey) {
-	if (collectionName) {
-		if (!this._collection[collectionName]) {
-			if (this.debug()) {
-				console.log('Creating collection ' + collectionName);
-			}
-		}
-
-		this._collection[collectionName] = this._collection[collectionName] || new Collection(collectionName).db(this);
-
-		if (primaryKey !== undefined) {
-			this._collection[collectionName].primaryKey(primaryKey);
-		}
-
-		return this._collection[collectionName];
-	} else {
-		throw('ForerunnerDB.Core "' + this.name() + '": Cannot get collection with undefined name!');
+Collection.prototype.feedIn = function (collection) {
+	if (typeof collection === 'string') {
+		// The collection passed is a name, not a reference so get
+		// the reference from the name
+		collection = this._db.collection(collection);
 	}
+
+
 };
+
+Core.prototype.collection = new Overload({
+	/**
+	 * Get a collection by name. If the collection does not already exist
+	 * then one is created for that name automatically.
+	 * @param {Object} options An options object.
+	 * @returns {Collection}
+	 */
+	'object': function (options) {
+		return this.$main.call(this, options);
+	},
+
+	/**
+	 * Get a collection by name. If the collection does not already exist
+	 * then one is created for that name automatically.
+	 * @param {String} collectionName The name of the collection.
+	 * @returns {Collection}
+	 */
+	'string': function (collectionName) {
+		return this.$main.call(this, {
+			name: collectionName
+		});
+	},
+
+	/**
+	 * Get a collection by name. If the collection does not already exist
+	 * then one is created for that name automatically.
+	 * @param {String} collectionName The name of the collection.
+	 * @param {String} primaryKey Optional primary key to specify the primary key field on the collection
+	 * objects. Defaults to "_id".
+	 * @returns {Collection}
+	 */
+	'string, string': function (collectionName, primaryKey) {
+		return this.$main.call(this, {
+			name: collectionName,
+			primaryKey: primaryKey
+		});
+	},
+
+	/**
+	 * Get a collection by name. If the collection does not already exist
+	 * then one is created for that name automatically.
+	 * @param {String} collectionName The name of the collection.
+	 * @param {Object} options An options object.
+	 * @returns {Collection}
+	 */
+	'string, object': function (collectionName, options) {
+		options.name = collectionName;
+
+		return this.$main.call(this, options);
+	},
+
+	/**
+	 * Get a collection by name. If the collection does not already exist
+	 * then one is created for that name automatically.
+	 * @param {String} collectionName The name of the collection.
+	 * @param {String} primaryKey Optional primary key to specify the primary key field on the collection
+	 * objects. Defaults to "_id".
+	 * @param {Object} options An options object.
+	 * @returns {Collection}
+	 */
+	'string, string, object': function (collectionName, primaryKey, options) {
+		options.name = collectionName;
+		options.primaryKey = primaryKey;
+
+		return this.$main.call(this, options);
+	},
+
+	/**
+	 * The main handler method. This get's called by all the other variants and
+	 * handles the actual logic of the overloaded method.
+	 * @param {Object} options An options object.
+	 * @returns {*}
+	 */
+	'$main': function (options) {
+		var name = options.name;
+
+		if (name) {
+			if (!this._collection[name]) {
+				if (options && options.autoCreate === false) {
+					throw('ForerunnerDB.Core "' + this.name() + '": Cannot get collection ' + name + ' because it does not exist and auto-create has been disabled!');
+				}
+
+				if (this.debug()) {
+					console.log('Creating collection ' + name);
+				}
+			}
+
+			this._collection[name] = this._collection[name] || new Collection(name).db(this);
+
+			if (options.primaryKey !== undefined) {
+				this._collection[name].primaryKey(options.primaryKey);
+			}
+
+			return this._collection[name];
+		} else {
+			throw('ForerunnerDB.Core "' + this.name() + '": Cannot get collection with undefined name!');
+		}
+	}
+});
 
 /**
  * Determine if a collection with the passed name already exists.
