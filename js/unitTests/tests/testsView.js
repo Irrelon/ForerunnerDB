9fcd91adec40fb1964c7dc608493814fe4b65ac5
irrelon <rob@irrelon.com>
2015-06-04 11:40:47 +0100

    Added test for reactor error on dropped node

--- js/unitTests/tests/testsView.js
+++ js/unitTests/tests/testsView.js
@@ -22,3 +22,33 @@ QUnit.test('View.from() :: Drop a view\'s underlying collection', function () {
 
 	base.dbDown();
 });
+
+QUnit.test('View.drop() :: Drop a view and check underlying collection doesn\'t still chain data to the dropped view', function () {
+	"use strict";
+	base.dbUp();
+
+	var coll = db.collection('test'),
+		view = db.view('test'),
+		from;
+
+	view.from(coll);
+
+	coll.insert({
+		moo: true
+	});
+
+	view.drop();
+
+	try {
+		coll.setData({
+			moo: true
+		});
+	} catch (e) {
+		strictEqual(true, false, 'Dropped view caused error: ' + e);
+	}
+
+	strictEqual(coll, coll, 'OK');
+
+	base.dbDown();
+});
+
