79aab5ec1a148ae0c15d9097281c54f1a45a8292
irrelon <rob@irrelon.com>
2015-07-13 13:41:31 +0100

    New version build 1.3.60

--- js/lib/Shared.js
+++ js/lib/Shared.js
@@ -6,7 +6,7 @@
  * @mixin
  */
 var Shared = {
-	version: '1.3.59',
+	version: '1.3.60',
 	modules: {},
 
 	_synth: {},
--- js/unitTests/lib/fdb-all.js
+++ js/unitTests/lib/fdb-all.js
@@ -10541,7 +10541,7 @@ module.exports = Rest;
  * @mixin
  */
 var Shared = {
-	version: '1.3.59',
+	version: '1.3.60',
 	modules: {},
 
 	_synth: {},
