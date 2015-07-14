ada46a483bf5a837d7a0330e5224e15fe297d6b2
irrelon <rob@irrelon.com>
2015-07-14 10:35:27 +0100

    Updated grid tests to ensure data binding is working with document wrappers

--- js/unitTests/tests/testsGrid.js
+++ js/unitTests/tests/testsGrid.js
@@ -1,5 +1,5 @@
 QUnit.module('Grid');
-ForerunnerDB.moduleLoaded('Grid', function () {
+ForerunnerDB.moduleLoaded(['Grid', 'Document'], function () {
 	QUnit.test('Grid() :: Instantiate a grid', function () {
 		base.dbUp();
 
@@ -45,9 +45,62 @@ ForerunnerDB.moduleLoaded('Grid', function () {
 		}]);
 
 		ok(grid, 'Grid active');
-		//grid.drop();
+		grid.drop();
 
-		//base.domDown();
-		//base.dbDown();
+		base.domDown();
+		base.dbDown();
+	});
+
+	QUnit.test('Grid() :: Bind a grid with a document wrapper', function () {
+		base.dbUp();
+		base.domUp();
+
+		var coll = db.collection('gridTest').truncate(),
+			view = db.view('gridTest').from(coll),
+			doc = db.document('gridTestDoc').setData({
+				myData: 'Custom data wrapping object working'
+			}),
+			grid = view.grid('#testTarget2', '#gridTable', {
+				$wrapIn: doc
+			});
+
+		coll.setData([{
+			firstName: 'Fred',
+			lastName: 'Jones',
+			age: 15
+		}, {
+			firstName: 'Jim',
+			lastName: 'Monsoon',
+			age: 34
+		}, {
+			firstName: 'Tilly',
+			lastName: 'Monsoon',
+			age: 52
+		}, {
+			firstName: 'Arbit',
+			lastName: 'Frogsbottom',
+			age: 63
+		}, {
+			firstName: 'Fred',
+			lastName: 'Frogsbottom',
+			age: 62
+		}]);
+
+		ok(grid, 'Grid active');
+
+		// Check the dom element is there
+		strictEqual($.trim($('#myData').text()), 'Custom data wrapping object working', 'Output is correct before change');
+
+		// Update the wrapper doc myData property and check dom again
+		doc.update({}, {
+			myData: 'New text'
+		});
+
+		strictEqual($.trim($('#myData').text()), 'New text', 'Output is correct after change');
+
+		grid.drop();
+
+		base.domDown();
+		base.dbDown();
 	});
 });
