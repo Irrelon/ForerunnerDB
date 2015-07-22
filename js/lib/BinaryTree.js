e515990bfa04759cd4f6119bfd3db3fd9c5f5f89
irrelon <rob@irrelon.com>
2015-07-22 14:35:12 +0100

    Binary tree update

--- js/lib/BinaryTree.js
+++ js/lib/BinaryTree.js
@@ -1,385 +1,209 @@
 "use strict";
 
-var TreeNode = function(tree, key, value) {
-	this.tree = tree;
-	this.key = key;
-	this.data = [value];
-	this.left = null;
-	this.right = null;
-	return this;
+var Shared = require('./Shared');
+
+var BinaryTree = function (data, compareFunc, hashFunc) {
+	this.init.apply(this, arguments);
 };
 
-TreeNode.prototype.insert = function(newNode) {
-	var result = this.tree._sort(newNode.key, this.key);
+BinaryTree.prototype.init = function (data, index, compareFunc, hashFunc) {
+	this._store = [];
 
-	if(result === -1) {
-		if(this.left === null) {
-			this.left = newNode;
+	if (index !== undefined) { this.index(index); }
+	if (compareFunc !== undefined) { this.compareFunc(compareFunc); }
+	if (hashFunc !== undefined) { this.hashFunc(hashFunc); }
+	if (data !== undefined) { this.data(data); }
+};
+
+Shared.addModule('BinaryTree', BinaryTree);
+Shared.mixin(BinaryTree.prototype, 'Mixin.ChainReactor');
+Shared.mixin(BinaryTree.prototype, 'Mixin.Sorting');
+Shared.mixin(BinaryTree.prototype, 'Mixin.Common');
+
+Shared.synthesize(BinaryTree.prototype, 'compareFunc');
+Shared.synthesize(BinaryTree.prototype, 'hashFunc');
+Shared.synthesize(BinaryTree.prototype, 'indexDir');
+Shared.synthesize(BinaryTree.prototype, 'index', function (index) {
+	if (index !== undefined) {
+		if (index[0]) {
+			this._indexProp = index[0].prop;
+			this._indexDir = index[0].dir;
 		} else {
-			this.left.insert(newNode);
-		}
-	} else if(result === 1) {
-		if(this.right === null) {
-			this.right = newNode;
-		} else {
-			this.right.insert(newNode);
-		}
-	} else {
-		return true;
-	}
-};
-
-TreeNode.prototype.search = function (query, resultObj) {
-	var result = this.tree._operationCompare(query, this.key);
-
-	// Increment node visit count
-	resultObj.nodes.push(this);
-
-	if(result.metric === 0) {
-		// Now scan both tree branches for more matches
-		if (this.left !== null) {
-			resultObj.path.push('left', this);
-			this.left.search(query, resultObj);
-		}
-
-		// We have a matching result, add it to results
-		if (result.match) {
-			// It is very important that we push here below the left-side traversal
-			// otherwise the order of the returned items will be incorrect
-			resultObj.path.push('result', this);
-			resultObj.arr.push(this.key);
-		}
-
-		if (this.right !== null) {
-			resultObj.path.push('right', this);
-			this.right.search(query, resultObj);
-		}
-	} else if(result.metric === -1 && this.left !== null) {
-		// This node does not match and we have been told to look left
-		resultObj.path.push('left', this);
-		this.left.search(query, resultObj);
-	} else if(result.metric === 1 && this.right !== null) {
-		// This node does not match and we have been told to look right
-		resultObj.path.push('right', this);
-		this.right.search(query, resultObj);
-	}
-};
-
-TreeNode.prototype.depthFirstSearch = function(search) {
-	var result = this.tree._sort(search, this.key);
-	console.log(search, ":", this.key);
-
-	if(result === 0) {
-		console.log("search item found");
-		return true;
-	} else if(result === -1 && this.left !== null) {
-		return this.left.depthFirstSearch(search);
-	} else if(result === 1 && this.right !== null) {
-		return this.right.depthFirstSearch(search);
-	} else {
-		console.log("could not find "+search);
-		return false;
-	}
-};
-
-
-TreeNode.prototype.inorderTraversal = function() {
-	if(this.left !== null) {
-		this.left.inorderTraversal();
-	}
-	console.log(this.key);
-	if(this.right !== null) {
-		this.right.inorderTraversal();
-	}
-};
-
-TreeNode.prototype.preOrderTraversal = function() {
-	console.log(this.key);
-	if(this.left !== null) {
-		this.left.preOrderTraversal();
-	}
-	if(this.right !== null) {
-		this.right.preOrderTraversal();
-	}
-};
-
-TreeNode.prototype.postOrderTraversal = function() {
-	if(this.left !== null) {
-		this.left.postOrderTraversal();
-	}
-	if(this.right !== null) {
-		this.right.postOrderTraversal();
-	}
-	console.log(this.key);
-};
-
-var BinarySearchTree = function(orderBy) {
-	var i, keyData;
-
-	// TODO when added to the main system, use shared ref to module
-	this.pathSolver = new window.ForerunnerDB.shared.modules.Path();
-
-	this._orderKeys = this.pathSolver.parseArr(orderBy);
-	this._orderKeyData = [];
-	this._orderKeyDataByKey = {};
-
-	for (i = 0; i < this._orderKeys.length; i++) {
-		keyData = {
-			key: this._orderKeys[i],
-			dir: this.pathSolver.get(orderBy, this._orderKeys[i]),
-			pathSolver: new window.ForerunnerDB.shared.modules.Path(this._orderKeys[i])
-		};
-
-		this._orderKeyData.push(keyData);
-		this._orderKeyDataByKey[this._orderKeys[i]] = keyData;
-	}
-};
-
-window.ForerunnerDB.shared.mixin(BinarySearchTree.prototype, 'Mixin.Matching');
-
-BinarySearchTree.prototype._sort = function (a, b) {
-	var aVal,
-		bVal,
-		arr = this._orderKeyData,
-		count = arr.length,
-		index,
-		sortData,
-		castType;
-
-	for (index = 0; index < count; index++) {
-		sortData = arr[index];
-
-		aVal = a[sortData.key];
-		bVal = b[sortData.key];
-
-		castType = typeof aVal;
-
-		if (castType === 'number') {
-			aVal = Number(aVal);
-			bVal = Number(bVal);
-		}
-
-		// Check for non-equal items
-		if (aVal !== bVal) {
-			// Return the sorted items
-			if (sortData.dir === 1) {
-				return this.sortAsc(aVal, bVal);
-			}
-
-			if (sortData.dir === -1) {
-				return this.sortDesc(aVal, bVal);
-			}
+			this._indexProp = null;
+			this._indexDir = 0;
 		}
 	}
 
-	return 0;
+	return this.$super.call(this, index);
+});
+
+BinaryTree.prototype.data = function (val) {
+	if (val !== undefined) {
+		this._data = val;
+
+		if (this._hashFunc) { this._hash = this._hashFunc(val); }
+		return this;
+	}
+
+	return this._data;
 };
 
-BinarySearchTree.prototype.sortAsc = function (a, b) {
-	if (typeof(a) === 'string' && typeof(b) === 'string') {
-		return a.localeCompare(b);
-	} else {
-		if (a > b) {
-			return 1;
-		} else if (a < b) {
-			return -1;
+BinaryTree.prototype.push = function (val) {
+	if (val !== undefined) {
+		this._store.push(val);
+		return this;
+	}
+
+	return false;
+};
+
+BinaryTree.prototype.pull = function (val) {
+	if (val !== undefined) {
+		var index = this._store.indexOf(val);
+
+		if (index > -1) {
+			this._store.splice(index, 1);
+			return true;
 		}
 	}
 
-	return 0;
+	return false;
 };
 
-BinarySearchTree.prototype.sortDesc = function (a, b) {
-	if (typeof(a) === 'string' && typeof(b) === 'string') {
-		return b.localeCompare(a);
-	} else {
-		if (a > b) {
-			return -1;
-		} else if (a < b) {
-			return 1;
+/**
+ * Default compare method. Can be overridden.
+ * @param a
+ * @param b
+ * @returns {number}
+ * @private
+ */
+BinaryTree.prototype._compareFunc = function (a, b) {
+	// Loop the index array
+	var i,
+		indexData,
+		result = 0;
+
+	for (i = 0; i < this._index.length; i++) {
+		indexData = this._index[i];
+
+		if (indexData.dir === 1) {
+			result = this.sortAsc(a[indexData.prop], b[indexData.prop]);
+		} else if (indexData.dir === -1) {
+			result = this.sortDesc(a[indexData.prop], b[indexData.prop]);
 		}
-	}
 
-	return 0;
-};
-
-BinarySearchTree.prototype.search = function (query) {
-	var resultObj = {
-			arr: [],
-			nodes: [],
-			path: []
-		},
-		searchKey = this.objKey(query);
-
-	// Check for no matching key values and return early if -1
-	if (searchKey === -1) {
-		return resultObj;
-	}
-
-	this.root.search(searchKey, resultObj);
-
-	return resultObj;
-};
-
-BinarySearchTree.prototype._operationCompare = function (a, b) {
-	var result = {
-			match: false,
-			metric: 0
-		},
-		aVal,
-		bVal,
-		index,
-		arr = this._orderKeyDataByKey,
-		sortData,
-		matchVal;
-
-	for (index in a) {
-		if (a.hasOwnProperty(index)) {
-			sortData = arr[index];
-
-			aVal = a[sortData.key];
-			bVal = b[sortData.key];
-
-			// Run operation on values
-			if (!this._match(bVal, aVal)) {
-				result.match = false;
-
-				// Get the value of the field without any operation
-				// keys getting in the way
-				if (typeof aVal === 'object') {
-					if (aVal.$gt || aVal.$gt) {
-						matchVal = aVal.$gt;
-						/*result.metric = -sortData.dir;
-						return result;*/
-					}
-
-					if (aVal.$gte) {
-						matchVal = aVal.$gte;
-						/*result.metric = sortData.dir;
-						return result;*/
-					}
-
-					if (aVal.$lt) {
-						matchVal = aVal.$lt;
-						/*result.metric = -sortData.dir;
-						return result;*/
-					}
-
-					if (aVal.$lte) {
-						matchVal = aVal.$lte;
-						/*result.metric = -sortData.dir;
-						return result;*/
-					}
-				} else {
-					matchVal = aVal;
-				}
-
-				// Return the sorted items
-				if (sortData.dir === 1) {
-					result.metric = this.sortAsc(matchVal, bVal);
-					return result;
-				}
-
-				if (sortData.dir === -1) {
-					result.metric = this.sortDesc(matchVal, bVal);
-					return result;
-				}
-			} else {
-				result.match = true;
-			}
+		if (result !== 0) {
+			return result;
 		}
 	}
 
 	return result;
 };
 
-// Generate a key from the passed object
-BinarySearchTree.prototype.objKey = function (obj) {
-	var key = {},
-		arr = this._orderKeyData,
-		count = arr.length,
-		index,
-		keyData,
-		keyVal,
-		gotVal = false;
+/**
+ * Default hash function. Can be overridden.
+ * @param obj
+ * @private
+ */
+BinaryTree.prototype._hashFunc = function (obj) {
+	return obj[this._indexProp];
+};
 
-	for (index = 0; index < count; index++) {
-		keyData = arr[index];
-		keyVal = keyData.pathSolver.get(obj);
+BinaryTree.prototype.insert = function (data) {
+	var result,
+		subIndex;
 
-		if (keyVal !== undefined) {
-			key[keyData.key] = keyVal;
-			gotVal = true;
+	if (!this._data) {
+		// Insert into this node (overwrite) as there is no data
+		this.data(data);
+		this.push(data);
+		return true;
+	}
+
+	result = this._compareFunc(this._data, data);
+
+	if (result === 0) {
+		// Equal to this node, push to node's data array
+		this.push(data);
+
+		return true;
+	}
+
+	if (result === -1) {
+		// Greater than this node
+		if (this._right) {
+			// Propagate down the right branch
+			this._right.insert(data);
 		} else {
+			// Assign to right branch
+			this._right = new BinaryTree(data, this._index, this._compareFunc, this._hashFunc);
+		}
+
+		return true;
+	}
+
+	if (result === 1) {
+		// Less than this node
+		if (this._left) {
+			// Propagate down the left branch
+			this._left.insert(data);
+		} else {
+			// Assign to left branch
+			this._left = new BinaryTree(data, this._index, this._compareFunc, this._hashFunc);
+		}
+
+		return true;
+	}
+
+	return false;
+};
+
+BinaryTree.prototype.lookup = function (data) {
+	var result = this._compareFunc(this._data, data);
+
+	if (result === 0) {
+		return this._store;
+	}
+
+	if (result === -1) {
+		return this._right.lookup(data);
+	}
+
+	if (result === 1) {
+		return this._left.lookup(data);
+	}
+};
+
+BinaryTree.prototype.inOrder = function (type, resultArr) {
+	resultArr = resultArr || [];
+
+	if (this._left) {
+		this._left.inOrder(type, resultArr);
+	}
+
+	switch (type) {
+		case 'hash':
+			resultArr.push(this._hash);
 			break;
-		}
+
+		case 'key':
+			resultArr.push(this._data);
+			break;
+
+		default:
+			resultArr.push({
+				key: this._key,
+				arr: this._store
+			});
+			break;
 	}
 
-	if (!gotVal) {
-		return -1;
+	if (this._right) {
+		this._right.inOrder(type, resultArr);
 	}
 
-	return key;
+	return resultArr;
 };
 
-BinarySearchTree.prototype.insert = function(insert) {
-	if (!this.root) {
-		if(insert instanceof TreeNode) {
-			this.root = insert;
-		} else {
-			this.root = new TreeNode(this, this.objKey(insert), insert);
-		}
-	} else {
-		if (insert instanceof TreeNode) {
-			this.root.insert(insert);
-		} else {
-			this.root.insert(new TreeNode(this, this.objKey(insert), insert));
-		}
-	}
-
-	return this;
-};
-
-BinarySearchTree.prototype.depthFirstSearch = function(searchValue) {
-	this.root.depthFirstSearch(searchValue);
-	return this;
-};
-
-/*BinarySearchTree.prototype.breadthFirstTraversal = function() {
-	console.log("Breadth First Traversal");
-
-	// For our intensive purposes,
-	// our array is acting as a queue for us.
-	var queue = [],
-		current = this.root;
-
-	if(current !== null) {
-		queue.push(current);
-	}
-
-	// start off enqueuing root
-	while(queue.length > 0) {
-		var tempNode = queue.shift();
-		console.log(tempNode.key); // Visit current node
-		if(tempNode.left !== null) {
-			queue.push(tempNode.left);
-		}
-		if(tempNode.right !== null) {
-			queue.push(tempNode.right);
-		}
-	}
-	return this;
-};*/
-
-BinarySearchTree.prototype.inOrderTraversal = function(){
-	this.root.inorderTraversal();
-	return this;
-};
-BinarySearchTree.prototype.preOrderTraversal = function(){
-	this.root.preOrderTraversal();
-	return this;
-};
-BinarySearchTree.prototype.postOrderTraversal = function(){
-	this.root.postOrderTraversal();
-	return this;
-};
+Shared.finishModule('BinaryTree');
+module.exports = BinaryTree;
