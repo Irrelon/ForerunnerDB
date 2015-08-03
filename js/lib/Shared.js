0b16e73ad06190e0e37c76942078d1174f1a08db
irrelon <rob@irrelon.com>
2015-08-03 11:20:55 +0100

    Added mixin overload so we can process bespoke objects

--- js/lib/Shared.js
+++ js/lib/Shared.js
@@ -1,12 +1,14 @@
 "use strict";
 
+var Overload = require('./Overload');
+
 /**
  * A shared object that can be used to store arbitrary data between class
  * instances, and access helper methods.
  * @mixin
  */
 var Shared = {
-	version: '1.3.133',
+	version: '1.3.138',
 	modules: {},
 
 	_synth: {},
@@ -68,19 +70,33 @@ var Shared = {
 	 * @param {Object} obj The target object to add mixin key/values to.
 	 * @param {String} mixinName The name of the mixin to add to the object.
 	 */
-	mixin: function (obj, mixinName) {
-		var system = this.mixins[mixinName];
+	mixin: new Overload({
+		'object, string': function (obj, mixinName) {
+			var mixinObj;
 
-		if (system) {
-			for (var i in system) {
-				if (system.hasOwnProperty(i)) {
-					obj[i] = system[i];
+			if (typeof mixinName === 'string') {
+				mixinObj = this.mixins[mixinName];
+
+				if (!mixinObj) {
+					throw('ForerunnerDB.Shared: Cannot find mixin named: ' + mixinName);
 				}
 			}
-		} else {
-			throw('ForerunnerDB.Shared: Cannot find mixin named: ' + mixinName);
+
+			return this.$main.call(this, obj, mixinObj);
+		},
+
+		'object, object': function (obj, mixinObj) {
+			return this.$main.call(this, obj, mixinObj);
+		},
+
+		'$main': function (obj, mixinObj) {
+			for (var i in mixinObj) {
+				if (mixinObj.hasOwnProperty(i)) {
+					obj[i] = mixinObj[i];
+				}
+			}
 		}
-	},
+	}),
 
 	/**
 	 * Generates a generic getter/setter method for the passed method name.
@@ -126,7 +142,7 @@ var Shared = {
 	 * @returns {Function}
 	 * @constructor
 	 */
-	overload: require('./Overload'),
+	overload: Overload,
 
 	/**
 	 * Define the mixins that other modules can use as required.
