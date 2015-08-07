4cfbc4188bc62d23552a1338929d5d21dbf1b0e6
irrelon <rob@irrelon.com>
2015-08-07 08:51:32 +0100

    New version build 1.3.170

--- js/dist/fdb-all.js
+++ js/dist/fdb-all.js
@@ -11139,7 +11139,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.168',
+	version: '1.3.170',
 	modules: {},
 
 	_synth: {},
--- js/lib/Shared.js
+++ js/lib/Shared.js
@@ -8,7 +8,7 @@ var Overload = require('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.168',
+	version: '1.3.170',
 	modules: {},
 
 	_synth: {},
