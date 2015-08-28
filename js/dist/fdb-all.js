516d9d989a788d1e45a7d50f5711a46fd68c4559
irrelon <rob@irrelon.com>
2015-08-28 14:43:52 +0100

    New version build 1.3.218

--- js/dist/fdb-all.js
+++ js/dist/fdb-all.js
@@ -11337,7 +11337,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.217',
+	version: '1.3.218',
 	modules: {},
 
 	_synth: {},
--- js/unitTests/lib/fdb-core+views.js
+++ js/unitTests/lib/fdb-core+views.js
@@ -8464,7 +8464,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.217',
+	version: '1.3.218',
 	modules: {},
 
 	_synth: {},
