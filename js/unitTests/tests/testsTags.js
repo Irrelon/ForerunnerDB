2504c153343cd40c6382364cfcc75965d01d95ff
irrelon <rob@irrelon.com>
2015-11-23 14:29:46 +0000

    Test async drops

--- js/unitTests/tests/testsTags.js
+++ js/unitTests/tests/testsTags.js
@@ -19,8 +19,9 @@ ForerunnerDB.moduleLoaded('Collection', function () {
 		base.dbDown();
 	});
 
-	QUnit.test('Collection.tagDrop() :: Add a tag to a collection', function () {
+	QUnit.asyncTest('Collection.tagDrop() :: Add a tag to a collection', function () {
 		"use strict";
+		expect(6);
 		base.dbUp();
 
 		var coll1 = db.collection('test1'),
@@ -30,15 +31,78 @@ ForerunnerDB.moduleLoaded('Collection', function () {
 		coll1.tagAdd('users/list');
 		coll2.tagAdd('users/list');
 
+		arr = db.tagLookup('users/list');
+
+		strictEqual(arr.length, 2, 'OK');
+
 		strictEqual(coll1.isDropped(), false, 'Collection 1 is not dropped');
 		strictEqual(coll2.isDropped(), false, 'Collection 2 is not dropped');
 
 		// Drop collections by tag
-		db.tagDrop('users/list');
+		db.tagDrop('users/list', function () {
+			strictEqual(coll1.isDropped(), true, 'Collection 1 is dropped');
+			strictEqual(coll2.isDropped(), true, 'Collection 2 is dropped');
 
-		strictEqual(coll1.isDropped(), true, 'Collection 1 is dropped');
-		strictEqual(coll2.isDropped(), true, 'Collection 2 is dropped');
+			arr = db.tagLookup('users/list');
+
+			strictEqual(arr.length, 0, 'OK');
+
+			start();
+			base.dbDown();
+		});
+	});
+});
+
+ForerunnerDB.moduleLoaded('View', function () {
+	QUnit.test('View.tagAdd() :: Add a tag to a view', function () {
+		"use strict";
+		base.dbUp();
+
+		var coll1 = db.view('test1'),
+			coll2 = db.view('test2'),
+			arr;
+
+		coll1.tagAdd('users/list');
+		coll2.tagAdd('users/list');
+
+		// Get the views by their tag
+		arr = db.tagLookup('users/list');
+
+		strictEqual(arr.length, 2, 'OK');
 
 		base.dbDown();
+	});
+
+	QUnit.asyncTest('View.tagDrop() :: Add a tag to a view', function () {
+		"use strict";
+		expect(6);
+		base.dbUp();
+
+		var coll1 = db.view('test1'),
+			coll2 = db.view('test2'),
+			arr;
+
+		coll1.tagAdd('users/list');
+		coll2.tagAdd('users/list');
+
+		arr = db.tagLookup('users/list');
+
+		strictEqual(arr.length, 2, 'OK');
+
+		strictEqual(coll1.isDropped(), false, 'View 1 is not dropped');
+		strictEqual(coll2.isDropped(), false, 'View 2 is not dropped');
+
+		// Drop views by tag
+		db.tagDrop('users/list', function () {
+			strictEqual(coll1.isDropped(), true, 'View 1 is dropped');
+			strictEqual(coll2.isDropped(), true, 'View 2 is dropped');
+
+			arr = db.tagLookup('users/list');
+
+			strictEqual(arr.length, 0, 'OK');
+
+			start();
+			base.dbDown();
+		});
 	});
 });
