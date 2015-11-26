bcc817d45dbc6a9f1f31f1c2775b5f9a814ecde8
irrelon <rob@irrelon.com>
2015-11-26 14:22:25 +0000

    Doc comment updates

--- js/lib/ReactorIO.js
+++ js/lib/ReactorIO.js
@@ -6,7 +6,9 @@ var Shared = require('./Shared');
  * Provides chain reactor node linking so that a chain reaction can propagate
  * down a node tree. Effectively creates a chain link between the reactorIn and
  * reactorOut objects where a chain reaction from the reactorIn is passed through
- * the reactorProcess before being passed to the reactorOut object.
+ * the reactorProcess before being passed to the reactorOut object. Reactor
+ * packets are only passed through to the reactorOut if the reactor IO method
+ * chainSend is used.
  * @param {*} reactorIn An object that has the Mixin.ChainReactor methods mixed
  * in to it. Chain reactions that occur inside this object will be passed through
  * to the reactorOut object.
