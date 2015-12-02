423679aff14a00c72188bcfbc5c39a69e44cc3f4
irrelon <rob@irrelon.com>
2015-12-02 12:46:13 +0000

    New version build 1.3.404

--- js/dist/fdb-legacy.js
+++ js/dist/fdb-legacy.js
@@ -13294,7 +13294,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.403',
+	version: '1.3.404',
 	modules: {},
 	plugins: {},
 
--- js/unitTests/lib/fdb-all.js
+++ js/unitTests/lib/fdb-all.js
@@ -12456,7 +12456,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.403',
+	version: '1.3.404',
 	modules: {},
 	plugins: {},
 
