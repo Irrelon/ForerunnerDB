974ee0342e68b57cb8c143b0ae283ac6a8f7b05f
irrelon <rob@irrelon.com>
2015-12-08 13:09:48 +0000

    New version build 1.3.423

--- js/lib/Shared.js
+++ js/lib/Shared.js
@@ -8,7 +8,7 @@ var Overload = require('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.422',
+	version: '1.3.423',
 	modules: {},
 	plugins: {},
 
--- js/unitTests/lib/fdb-core.js
+++ js/unitTests/lib/fdb-core.js
@@ -8685,7 +8685,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.422',
+	version: '1.3.423',
 	modules: {},
 	plugins: {},
 
