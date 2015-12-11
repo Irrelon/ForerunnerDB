9034be8251ae8e529739b47064586490b8f884cd
irrelon <rob@irrelon.com>
2015-12-11 10:21:31 +0000

    Added debug log output when an overloaded method fails to find a matching signature. The method data is output which helps to identify which overloaded method threw the error.

--- js/lib/Overload.js
+++ js/lib/Overload.js
@@ -100,6 +100,7 @@ var Overload = function (def) {
 			}
 
 			name = typeof this.name === 'function' ? this.name() : 'Unknown';
+			console.log('Overload: ', def);
 			throw('ForerunnerDB.Overload "' + name + '": Overloaded method does not have a matching signature for the passed arguments: ' + this.jStringify(arr));
 		};
 	}
