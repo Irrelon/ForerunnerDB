073c3b940b31189053bd37847a44155fdf5eda47
irrelon <rob@irrelon.com>
2016-01-04 22:39:02 +0000

    Api test script updated to work with latest changes to REST api. All working nicely.

--- js/useTests/nodeApiServer.js
+++ js/useTests/nodeApiServer.js
@@ -4,6 +4,10 @@ var ForerunnerDB = require('../builds/nodecore'),
 	fdb = new ForerunnerDB(),
 	db = fdb.db('testApi');
 
+db.debug(true);
+db.persist.dataDir('./data');
+db.persist.auto(true);
+
 db.api.access('books', '*', function (modelName, methodName, req, callback) {
 	callback(false, modelName, methodName, req);
 });
