cfd02680ff3a3dae1bde45fee7b2ba9fb5afe534
irrelon <rob@irrelon.com>
2016-01-08 11:30:59 +0000

    New version build 1.3.514

--- ionicExampleClient/www/lib/forerunnerdb/js/dist/fdb-core+views.js
+++ ionicExampleClient/www/lib/forerunnerdb/js/dist/fdb-core+views.js
@@ -9787,7 +9787,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.513',
+	version: '1.3.514',
 	modules: {},
 	plugins: {},
 
--- js/unitTests/lib/fdb-core.js
+++ js/unitTests/lib/fdb-core.js
@@ -9175,7 +9175,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.513',
+	version: '1.3.514',
 	modules: {},
 	plugins: {},
 
