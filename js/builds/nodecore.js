4e17b49d26c721bb7826b4b8910989d500f18aff
irrelon <rob@irrelon.com>
2016-01-19 22:12:39 +0000

    Added all extension classes to Node.js core

--- js/builds/nodecore.js
+++ js/builds/nodecore.js
@@ -1,4 +1,9 @@
 var Core = require('./core'),
+	CollectionGroup = require('../lib/CollectionGroup'),
+	View = require('../lib/View'),
+	Document = require('../lib/Document'),
+	Overview = require('../lib/Overview'),
+	BinaryLog = require('../lib/BinaryLog'),
 	NodePersist = require('../lib/NodePersist'),
 	NodeApiServer = require('../lib/NodeApiServer');
 
