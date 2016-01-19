70db603ab2ab2531351970f2acfef4ed30fd4c7a
irrelon <rob@irrelon.com>
2016-01-19 22:21:40 +0000

    New version build 1.3.554

--- ionicExampleClient/www/lib/forerunnerdb/js/dist/fdb-all.js
+++ ionicExampleClient/www/lib/forerunnerdb/js/dist/fdb-all.js
@@ -14050,7 +14050,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.553',
+	version: '1.3.554',
 	modules: {},
 	plugins: {},
 
--- js/dist/fdb-legacy.js
+++ js/dist/fdb-legacy.js
@@ -14808,7 +14808,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.553',
+	version: '1.3.554',
 	modules: {},
 	plugins: {},
 
