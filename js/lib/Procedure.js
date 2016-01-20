86ba3bcafd616c7f40fe798b0267fa2dbc90ecc8
irrelon <rob@irrelon.com>
2016-01-20 09:59:56 +0000

    Added new Procedure class to handle incoming RPC on the server side

---
+++ js/lib/Procedure.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var Shared,
+	Db,
+	Procedure;
+
+Shared = require('./Shared');
+
+/**
+ * Create a constructor method that calls the instance's init method.
+ * This allows the constructor to be overridden by other modules because
+ * they can override the init method with their own.
+ */
+Procedure = function () {
+	this.init.apply(this, arguments);
+};
+
+Procedure.prototype.init = function (name, method) {
+	this._name = name;
+	this._method = method;
+};
+
+// Tell ForerunnerDB about our new module
+Shared.addModule('Procedure', Procedure);
+
+// Mixin some commonly used methods
+Shared.mixin(Procedure.prototype, 'Mixin.Common');
+Shared.mixin(Procedure.prototype, 'Mixin.Events');
+
+/**
+ * Get / set the procedure name.
+ * @name name
+ * @param {String=} name The name to set.
+ */
+Shared.synthesize(Procedure.prototype, 'name');
+
+/**
+ * Execute the procedure, passing in the request and response
+ * (req and res) arguments from the server. Procedure methods
+ * are responsible for correctly communicating with the client
+ * and handling response properly.
+ * @param req
+ * @param res
+ * @returns {*}
+ */
+Procedure.prototype.exec = function (req, res) {
+	return this._method.call(this, req, res);
+};
+
+// Grab the collection module for use later
+Db = Shared.modules.Db;
+
+/**
+ * Create or retrieve a procedure by name.
+ * @param {String} name The name of the procedure.
+ * @param {Function=} method If specified, creates a new procedure
+ * with the provided name and method.
+ * @returns {*}
+ */
+Db.prototype.procedure = function (name, method) {
+	var self = this;
+
+	if (name !== undefined) {
+		if (name !== undefined) {
+			if (self.debug()) {
+				console.log(self.logIdentifier() + ' Creating procedure ' + name);
+			}
+
+			self._procedure[name] = new Procedure(name, method);
+			self.emit('create', self._procedure[name], 'procedure', name);
+		}
+
+		return self._procedure[name];
+	}
+};
+
+// Tell ForerunnerDB that our module has finished loading
+Shared.finishModule('Procedure');
+module.exports = Procedure;
