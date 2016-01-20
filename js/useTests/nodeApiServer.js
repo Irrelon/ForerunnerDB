38a605f2aa6479d1ae3d83deed544ac4b311a1d2
irrelon <rob@irrelon.com>
2016-01-20 10:50:38 +0000

    Added user login and session

--- js/useTests/nodeApiServer.js
+++ js/useTests/nodeApiServer.js
@@ -4,14 +4,13 @@ var ForerunnerDB = require('../builds/nodecore'),
 	fdb = new ForerunnerDB(),
 	db = fdb.db('testApi');
 
-db.view('booksView')
-	.queryData({
-		enabled: true
-	})
-	.from('books');
-
 // Enable database debug logging to the console
 db.debug(true);
+
+// Set encryption on the persisted data
+db.persist.addStep(new db.shared.plugins.FdbCrypto({
+	pass: "testing"
+}));
 
 // Set the persist plugin's data folder
 db.persist.dataDir('./data');
@@ -19,20 +18,69 @@ db.persist.dataDir('./data');
 // Tell the database to load and save data for collections automatically
 db.persist.auto(true);
 
+db.view('booksView')
+	.queryData({
+		enabled: true
+	})
+	.from('books');
+
+// Create a remote procedure called "login" that checks for a user
+// and then if found, creates a new session and returns the session
+// details to the client.
+db.procedure('login', function (req, res) {
+	var email = req.json.email,
+		password = req.json.password,
+		result,
+		sessionInsert;
+
+	result = db.collection('user').findOne({
+		email: email,
+		password: password
+	});
+
+	if (result) {
+		// Create a new session for this user
+		sessionInsert = db.collection('session').insert({
+			userId: result._id,
+			createdTs: new Date()
+		});
+
+		if (sessionInsert.inserted[0]) {
+			// Send to client
+			res.status(200).send(sessionInsert.inserted[0]);
+		} else {
+			res.status(500).send('Session failure');
+		}
+	} else {
+		res.status(404).send('Authentication failed');
+	}
+});
+
 // Set access control to allow all HTTP verbs (*) on the "books" collection in the "testApi" database
 // db.api.access(<database name>, <object type>, <object name>, <http verb>, <your control method>);
 fdb.api.access('testApi', 'collection', 'books', '*', function (collectionName, methodName, req, callback) {
 	callback(false, collectionName, methodName, req);
 });
 
-fdb.api.access('testApi', 'view', '*', '*', function (viewName, methodName, req, callback) {
-	callback(false, viewName, methodName, req);
-});
+// Instead of passing a control method, you can use the strings "allow" and "deny" instead.
+// In this line we allow full access to the session collection (a very bad security risk)
+// for testing purposes. DO NOT USE THIS IN PRODUCTION!!!!!!
+fdb.api.access('testApi', 'collection', 'session', '*', 'allow');
+
+// Allow all HTTP verbs on all views
+fdb.api.access('testApi', 'view', '*', '*', 'allow');
+
+// Allow all clients get access to the login remote procedure
+fdb.api.access('testApi', 'procedure', 'login', 'GET', 'allow');
 
 // Ask the API server to start listening
-fdb.api.start('0.0.0.0', '9010', {cors: true}, function () {
-	console.log('Server started!');
-});
+fdb.api.start('0.0.0.0', '9010', {cors: true});
 
 // Stop API server with stop() call
 //fdb.api.stop();
+
+// Create a user in the user collection so that login procedure can be tested
+db.collection('user').insert({
+	email: 'test@test.com',
+	password: 'testing'
+});
