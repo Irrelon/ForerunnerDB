8cad7a779c69633dcd46ea00170765444b2b0557
irrelon <rob@irrelon.com>
2016-01-20 12:11:03 +0000

    JSDoc updates

--- js/lib/Procedure.js
+++ js/lib/Procedure.js
@@ -1,20 +1,29 @@
 "use strict";
 
+/**
+ * This class handles remote procedure call generation. It is an
+ * extension of the NodeApiServer class and is primarily used there.
+ * It allows procedures to be created that are exposed to remote
+ * clients via the REST interface. Procedures handle their own server
+ * code including responding to clients directly.
+ * @name Procedure
+ * @class
+ */
 var Shared,
 	Db,
 	Procedure;
 
 Shared = require('./Shared');
 
-/**
- * Create a constructor method that calls the instance's init method.
- * This allows the constructor to be overridden by other modules because
- * they can override the init method with their own.
- */
 Procedure = function () {
 	this.init.apply(this, arguments);
 };
 
+/**
+ * Create a remote procedure call.
+ * @param {String} name The name of the procedure.
+ * @param {Function} method The procedure handler.
+ */
 Procedure.prototype.init = function (name, method) {
 	this._name = name;
 	this._method = method;
