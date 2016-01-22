85ad51f151e0a211e190789b114fc626f5360619
irrelon <rob@irrelon.com>
2016-01-22 00:56:16 +0000

    Added selective auto-persist by type and name

--- js/lib/NodePersist.js
+++ js/lib/NodePersist.js
@@ -66,17 +66,52 @@ Shared.synthesize(NodePersist.prototype, 'mode');
  * will automatically load data for collections the first time they are
  * accessed and save data whenever it changes. This is disabled by
  * default.
- * @param {Boolean} val Set to true to enable, false to disable
+ * @param {Boolean} val Set to true to enable, false to disable.
+ * @param {Object=} objNames Optional object of type keys with an array
+ * of object names under them for which auto-persist is enabled.
  * @returns {*}
  */
-Shared.synthesize(NodePersist.prototype, 'auto', function (val) {
+NodePersist.prototype.auto = function (val, objNames) {
 	var self = this;
 
 	if (val !== undefined) {
 		if (val) {
+			if (objNamesArr) {
+				this._autoNames = objNames;
+			}
+
 			// Hook db events
-			this._db.on('create', function () { self._autoLoad.apply(self, arguments); });
-			this._db.on('change', function () { self._autoSave.apply(self, arguments); });
+			this._db.on('create', function (obj, objType, name) {
+				var arr;
+
+				if (!self._autoNames) {
+					self._autoLoad.call(self, obj, objType, name);
+				} else {
+					// Get list of names
+					arr = self._autoNames[objType];
+
+					// Check if this object is in the list of objects to auto-persist
+					if (name in arr) {
+						self._autoLoad.call(self, obj, objType, name);
+					}
+				}
+			});
+
+			this._db.on('change', function (obj, objType, name) {
+				var arr;
+
+				if (!self._autoNames) {
+					self._autoSave.call(self, obj, objType, name);
+				} else {
+					// Get list of names
+					arr = self._autoNames[objType];
+
+					// Check if this object is in the list of objects to auto-persist
+					if (name in arr) {
+						self._autoSave.call(self, obj, objType, name);
+					}
+				}
+			});
 
 			if (this._db.debug()) {
 				console.log(this._db.logIdentifier() + ' Automatic load/save enabled');
@@ -90,10 +125,13 @@ Shared.synthesize(NodePersist.prototype, 'auto', function (val) {
 				console.log(this._db.logIdentifier() + ' Automatic load/save disbled');
 			}
 		}
+
+		this._auto = val;
+		return this;
 	}
 
-	return this.$super.call(this, val);
-});
+	return this._auto;
+};
 
 NodePersist.prototype._autoLoad = function (obj, objType, name) {
 	var self = this;
