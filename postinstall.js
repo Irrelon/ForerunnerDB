82b202be753712dc03c408a63009bfbffee42fdf
irrelon <rob@irrelon.com>
2016-03-07 13:21:37 +0000

    Added post install script

---
+++ postinstall.js
@@ -0,0 +1,5 @@
+var package = require('./package.json');
+
+console.log('ForerunnerDB version ' + package.version + ' installed. ForerunnerDB is developed with ❤ love by Irrelon Software Limited, a UK registered company.');
+console.log('You can find support or ask questions here: https://github.com/Irrelon/ForerunnerDB/issues');
+console.log('We don\'t make any money from this project or ask for anything in return. If you like ForerunnerDB please take the time to star it on GitHub and show your support!');
