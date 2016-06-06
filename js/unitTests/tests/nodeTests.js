6192c032a48c6d69b6106593083084980ebdc654
irrelon <rob@irrelon.com>
2016-06-06 15:17:46 +0100

    Added Index2d test to testbear as it was causing TravisCI to fail... not sure why, might be because we are loading the data via a getJSON request?

--- js/unitTests/tests/nodeTests.js
+++ js/unitTests/tests/nodeTests.js
@@ -218,4 +218,86 @@ TB.test('Timed save, 50,000 records', function (callback) {
 	});
 });
 
+TB.test('Collection.index() :: Test 2d index search on large data set', function (callback) {
+	var fdb = new ForerunnerDB(),
+		db = fdb.db('temp'),
+		coll = db.collection('cities').truncate(),
+		result1,
+		result2,
+		cityData;
+
+	cityData = require('../data/cities.json');
+
+	coll.ensureIndex({
+		lngLat: 1
+	}, {
+		name: 'cityLatLngIndex',
+		type: '2d'
+	});
+
+	console.log('Inserting records: ' + cityData.length);
+
+	coll.insert(cityData, function () {
+		console.log('Collection record count: ' + coll.count());
+
+		var index = coll.index('cityLatLngIndex');
+
+		TB.ok(index !== undefined, "Check index is available: " + index.name());
+		TB.ok(index.name() === 'cityLatLngIndex', "Check index is correct name: " + index.name());
+
+		// Query index by distance
+		// $near queries are sorted by distance from center point by default
+		result1 = coll.find({
+			lngLat: {
+				$near: {
+					$point: [51.50722, -0.12750],
+					$maxDistance: 50,
+					$distanceUnits: 'miles',
+					$distanceField: 'dist',
+					$geoHashField: 'geoHash'
+				}
+			}
+		});
+
+		result2 = coll.find({
+			lngLat: {
+				$near: {
+					$point: [51.50722, -0.12750],
+					$maxDistance: 100,
+					$distanceUnits: 'miles',
+					$distanceField: 'dist',
+					$geoHashField: 'geoHash'
+				}
+			}
+		});
+
+		TB.strictEqual(result1.length < result2.length, true, 'Number of doc in 100 miles is more than docs in 50 miles');
+
+		TB.strictEqual(result1.length, 9, 'Result1 count correct');
+		TB.strictEqual(result2.length, 22, 'Result2 count correct');
+
+		TB.strictEqual(result1[0].name, 'London, UK', 'Result 1 correct');
+		TB.strictEqual(result1[1].name, 'Wycombe, Swanley, Greater London BR8, UK', 'Result 2 correct');
+		TB.strictEqual(result1[2].name, 'Basildon, Essex, UK', 'Result 3 correct');
+		TB.strictEqual(result1[3].name, 'Luton, UK', 'Result 4 correct');
+		TB.strictEqual(result1[4].name, 'Chelmsford, Essex, UK', 'Result 5 correct');
+		TB.strictEqual(result1[5].name, 'Southend-on-Sea, UK', 'Result 6 correct');
+		TB.strictEqual(result1[6].name, 'Aylesbury, Buckinghamshire, UK', 'Result 7 correct');
+		TB.strictEqual(result1[7].name, 'Milton Keynes, UK', 'Result 8 correct');
+		TB.strictEqual(result1[8].name, 'Brighton, Brighton and Hove, UK', 'Result 9 correct');
+
+		TB.strictEqual(result2[0].name, 'London, UK', 'Result 1 correct');
+		TB.strictEqual(result2[1].name, 'Wycombe, Swanley, Greater London BR8, UK', 'Result 2 correct');
+		TB.strictEqual(result2[2].name, 'Basildon, Essex, UK', 'Result 3 correct');
+		TB.strictEqual(result2[3].name, 'Luton, UK', 'Result 4 correct');
+		TB.strictEqual(result2[4].name, 'Chelmsford, Essex, UK', 'Result 5 correct');
+		TB.strictEqual(result2[5].name, 'Southend-on-Sea, UK', 'Result 6 correct');
+		TB.strictEqual(result2[6].name, 'Aylesbury, Buckinghamshire, UK', 'Result 7 correct');
+		TB.strictEqual(result2[7].name, 'Milton Keynes, UK', 'Result 8 correct');
+		TB.strictEqual(result2[8].name, 'Brighton, Brighton and Hove, UK', 'Result 9 correct');
+
+		callback();
+	});
+});
+
 TB.start();
