cc57d5fe4ce84d9c3ba94d625ea222caa26c8967
irrelon <rob@irrelon.com>
2016-09-16 15:31:44 +0100

    New version build 1.3.897

--- js/dist/fdb-core+persist.js
+++ js/dist/fdb-core+persist.js
@@ -12659,7 +12659,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.896',
+	version: '1.3.897',
 	modules: {},
 	plugins: {},
 	index: {},
--- js/lib/Shared.js
+++ js/lib/Shared.js
@@ -8,7 +8,7 @@ var Overload = require('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.896',
+	version: '1.3.897',
 	modules: {},
 	plugins: {},
 	index: {},
