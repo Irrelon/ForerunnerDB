535f66fc19be9f64d14983b7e599a8bc292d2faf
irrelon <rob@irrelon.com>
2016-09-20 12:07:26 +0100

    New version build 1.3.900

--- js/perfTests/lib/fdb-all.js
+++ js/perfTests/lib/fdb-all.js
@@ -15938,7 +15938,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.899',
+	version: '1.3.900',
 	modules: {},
 	plugins: {},
 	index: {},
--- js/perfTests/lib/fdb-legacy.js
+++ js/perfTests/lib/fdb-legacy.js
@@ -16302,7 +16302,7 @@ var Overload = _dereq_('./Overload');
  * @mixin
  */
 var Shared = {
-	version: '1.3.899',
+	version: '1.3.900',
 	modules: {},
 	plugins: {},
 	index: {},
